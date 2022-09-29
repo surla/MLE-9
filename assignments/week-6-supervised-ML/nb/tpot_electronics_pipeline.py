34e0f73b5b8ac5d7b4cd505758020de9d0300892
Joseph Surla <josephsurla@gmail.com>
2022-09-29 15:16:19 -0700

    week-6 assignment complete

--- assignments/week-6-supervised-ML/nb/tpot_electronics_pipeline.py
+++ assignments/week-6-supervised-ML/nb/tpot_electronics_pipeline.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import FastICA
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from xgboost import XGBClassifier
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,8 +14,8 @@ training_features, testing_features, training_target, testing_target = \
 
 # Average CV score on the training set was: 1.0
 exported_pipeline = make_pipeline(
-    FastICA(tol=0.30000000000000004),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=2, min_samples_split=5, n_estimators=100)
+    FastICA(tol=0.35000000000000003),
+    XGBClassifier(learning_rate=0.5, max_depth=10, min_child_weight=10, n_estimators=100, n_jobs=1, subsample=1.0, verbosity=0)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
